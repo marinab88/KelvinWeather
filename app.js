9df87133aab1f98086cf33d10c1333b85bfd1c65
Marina <bucsamarina03@gmail.com>
2021-04-28 22:52:08 +0300

    Used corectly the .floor method to round down the Fahrenheit temperature.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const kelvin=293; // The forecast today is 293Kelvin
+const kelvin=294; // The forecast today is 293Kelvin
 let celsius=kelvin-273; //Celsius is 273 degrees less than Kelvin
-let Fahrenheit=celsius*(9/5)+32; //Convert from Celsius to Fahrenheit
-
-Math.floor(Fahrenheit); //round down the Fahrenheit temperature
+let Fahrenheit=Math.floor(celsius*(9/5)+32); //Convert from Celsius to Fahrenheit
+//Math.floor() round down the Fahrenheit temperature
 
 console.log(`The temperature is ${Fahrenheit} degrees Fahrenheit.`);
